62e271013b7ef9612a7bbabde8ab227483f64478
Abdullahbdbd <121636151+Abdullahbdbd@users.noreply.github.com>
2025-11-09 23:02:30 +0600

    Connect Mongodb

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port =process.env.PORT || 3000;
 
@@ -7,9 +9,35 @@ const port =process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
+// CineVerse
+// ivC2PIsv18ztdtAs
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.nmjlal4.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } 
+  finally {
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
